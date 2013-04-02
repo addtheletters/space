304eebe89e97af3a47b54beb09edaf9ecbfc7c82
lexical_enumeration <bzhang.2003@gmail.com>
2013-04-01 22:09:43 -0400

    Tiny updates to RCTurning
    Getting ready to do actual work XD

--- src/atl/space/entities/RCTurningComponent.java
+++ src/atl/space/entities/RCTurningComponent.java
@@ -5,18 +5,19 @@ import java.util.List;
 import org.lwjgl.util.vector.Vector3f;
 
 public class RCTurningComponent extends RTurningComponent {
-	private Vector3f forwardStorage;
 	private boolean turning180 = false;
+	private Vector3f target;
 	
 	public void initiate180(){
 		FacingComponent fc = (FacingComponent)owner.getComponent("facing");
-		forwardStorage = fc.facing;
+		fc.facing.negate(target); //not sure if this will work or throw null pointers
 		turning180 = true;
 	}
 	
 	public void quit180(){
 		turning180 = false;
 	}
+	
 	public void update(int delta, List<Entity> entities) {
 		if(turning180 = false){
 			super.update(delta, entities);
@@ -30,7 +31,12 @@ public class RCTurningComponent extends RTurningComponent {
 			Entity.restrictLength(fc.facing, 1);	
 		}
 	}
+	public void setTarget(Vector3f trgt){
+		target = trgt;
+	}
+	
 	public Vector3f getStrongTurn(){
+		
 		//TODO: make this work
 		return null;
 	}
