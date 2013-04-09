d3a40c697576cf9a69964332450819626d253aef
Zach Wade <zacharywade@gmail.com>
2013-04-09 13:47:15 -0400

    working -sorta

--- src/atl/space/entities/TargetingOverlayRenderComponent.java
+++ src/atl/space/entities/TargetingOverlayRenderComponent.java
@@ -25,6 +25,10 @@ public class TargetingOverlayRenderComponent extends EquiTriangleOverlayRenderCo
 	private Color color;
 	private int size;
 	
+	private Texture texture = null;
+	
+	private boolean shouldRender = false;
+	
 	public TargetingOverlayRenderComponent(Color color) {
 		super(color, 20);
 		
@@ -45,14 +49,19 @@ public class TargetingOverlayRenderComponent extends EquiTriangleOverlayRenderCo
 		if (winpos.get(2) > 0 && winpos.get(2) < 1) {
 			setUp2D();
 			
-			renderImage("wat.png","PNG",0,0);
 			if ( Math.abs( (int)(mouseX-windowX) ) < size && Math.abs( (int)(mouseY-windowY) ) < size) {
 				if (Mouse.isButtonDown(0)) {
-					renderBox(windowX,windowY);
+					shouldRender = true;
+					renderImage("lena.jpg","JPG",0,0);
+					//renderBox(windowX,windowY);
 				}
 				renderHighlight(windowX, windowY);
 			} else {
 				renderTriangle(windowX, windowY);
+			}
+			
+			if (shouldRender) {
+				renderImage("lena.jpg","JPG",0,0);
 			}
 			
 			backTo3D();
@@ -111,10 +120,13 @@ public class TargetingOverlayRenderComponent extends EquiTriangleOverlayRenderCo
 		glEnd();
 	}
 	public void renderImage(String uri, String format, int x, int y) {
-		Texture texture = null;
+		
 		try {
-			texture = TextureLoader.getTexture(format, new FileInputStream(new File(uri)));
-		        // Replace PNG with your file extension
+			if (texture==null) {
+				texture = TextureLoader.getTexture(format, new FileInputStream(new File(uri)));
+			} else {
+				//System.out.println(texture);
+			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 			Display.destroy();
@@ -124,15 +136,20 @@ public class TargetingOverlayRenderComponent extends EquiTriangleOverlayRenderCo
 			Display.destroy();
 			System.exit(1);
 		}
+		
+		texture.bind();
+		
 		GL11.glBegin(GL11.GL_QUADS);
-		GL11.glTexCoord2f(0,0);
-		GL11.glVertex2f(100,100);
-		GL11.glTexCoord2f(1,0);
-		GL11.glVertex2f(100+texture.getTextureWidth(),100);
+		
 		GL11.glTexCoord2f(1,1);
-		GL11.glVertex2f(100+texture.getTextureWidth(),100+texture.getTextureHeight());
+		GL11.glVertex2f(000,000);
 		GL11.glTexCoord2f(0,1);
-		GL11.glVertex2f(100,100+texture.getTextureHeight());
+		GL11.glVertex2f(200,000);
+		GL11.glTexCoord2f(0,0);
+		GL11.glVertex2f(200,200);
+		GL11.glTexCoord2f(1,0);
+		GL11.glVertex2f(000,200);
+		
 		GL11.glEnd();
 	}
 }
