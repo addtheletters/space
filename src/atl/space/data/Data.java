35ffc890f03112f0e573a059a976d7a0b7c648de
lexical_enumeration <bzhang.2003@gmail.com>
2013-05-09 10:49:06 -0400

    Data musings, cont

--- src/atl/space/data/Data.java
+++ src/atl/space/data/Data.java
@@ -5,10 +5,21 @@ public abstract class Data {
 	 * 	I still don't really know if this is how I want to do this. Help?
 	 */
 	
-	
+	//I could go for realism and have the data be in the form of a bytearray or something.
+	//Seems like it'd be simpler to have various kinds of return and just have the processor
+	//handle them differently based on the identifier.
 	public abstract Object getData();
-	public abstract boolean isLocationData();
-	public abstract boolean isEntityData();
-	public abstract boolean isBasicData();
-	public abstract boolean isCommData();
+	//public abstract boolean isLocationData();
+	//public abstract boolean isEntityData();
+	//public abstract boolean isBasicData();
+	//public abstract boolean isCommData();
+	
+	/*
+	 * replace the boolean craps?
+	 */
+	public String identifier(){
+		return "UNIDENTIFIED";
+	}
+	
+
 }
