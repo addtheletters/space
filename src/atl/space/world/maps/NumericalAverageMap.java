fe273ed09e046501569c396289c23bd2e16657ff
addtheletters <bzhang.2003@gmail.com>
2013-05-14 16:49:18 -0300

    Create NumericalAverageMap.java

---
+++ src/atl/space/world/maps/NumericalAverageMap.java
@@ -0,0 +1,11 @@
+package atl.space.world.maps;
+
+import atl.space.entities.Entity;
+
+public class NumericalAverageMap extends AbstractMap{
+  public NumericalAverageMap(List<Entity> entities){
+    
+  }
+
+
+}
