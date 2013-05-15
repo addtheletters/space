239acf70063c21335d4dfaf89cd105a14508aed7
addtheletters <bzhang.2003@gmail.com>
2013-05-15 09:46:46 -0300

    Parameter!

--- src/atl/space/world/maps/AbstractMap.java
+++ src/atl/space/world/maps/AbstractMap.java
@@ -2,17 +2,17 @@ package atl.space.world.maps;
 
 import org.lwjgl.util.vector.Vector3f;
 
-public abstract class AbstractMap{
+public abstract class AbstractMap<T>{
   //Not exactly sure how to do this
   //Basically we want it set up to be able to track some sort of data for every point in space.
   //Eg: Heat. You should be able to look at any point in space and determine the heat(for the world), 
                                                                 //or guess what the heat is(for an observer)
-  Map<Vector3f, Object> vitalPoints;
-  Object[][][] background;
+  Map<Vector3f, T> vitalPoints;
+  T[][][] background;
   
   //Constructor based on entities?
   
-  public Object getValue(Vector3f location){
+  public T getValue(Vector3f location){
     return getValue(location.getX(), location.getY(), location.getZ());
   }
   
@@ -20,9 +20,9 @@ public abstract class AbstractMap{
   /*
   * Uses data in background to obtain a value for the coords. Maybe also uses vitalPoints?
   */
-  public abstract Object getValue(double x, double y, double z);
+  public abstract T getValue(double x, double y, double z);
   
-  public void addVitalPoint(Vector3f location, Object value){
+  public void addVitalPoint(Vector3f location, T value){
     vitalPoints.put(location, value);
     adjustBackground();
   }
