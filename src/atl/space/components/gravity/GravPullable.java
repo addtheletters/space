ac0acad8a4381646b3e946de5b646d3cb447b365
addtheletters <bzhang.2003@gmail.com>
2013-05-21 16:21:02 -0300

    Update GravPullable.java

--- src/atl/space/components/gravity/GravPullable.java
+++ src/atl/space/components/gravity/GravPullable.java
@@ -4,6 +4,5 @@ public interface GravPullable{
   
   public double getGravMass();
   public void applyPull();
-  public void applyPull(double modifier);
-  
+  //modifiers will actually modify what getGravMass returns, not factoring applyPull
 }
