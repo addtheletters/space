a9af02f14420b133de11df9d7408470d355ac8ed
addtheletters <bzhang.2003@gmail.com>
2013-05-21 15:50:51 -0300

    Gravity: PullComponent

---
+++ src/atl/space/components/gravity/GravPullComponent.java
@@ -0,0 +1,15 @@
+package atl.space.components.gravity;
+
+public class GravPullComponent implements GravPullable{
+  public double gravMass;
+  public GravPullComponent(){
+  
+  }
+  public GravPullComponent(double mass){
+    
+  }
+  public GravPullComponent(GravPullComponent gpc){
+    
+  }
+  
+}
