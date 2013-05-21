9289d2ecc2382d26c84fd1768a3e67d89808eb89
addtheletters <bzhang.2003@gmail.com>
2013-05-21 16:31:26 -0300

    Gravity: Moving 'mass'

--- src/atl/space/components/gravity/GravPullComponent.java
+++ src/atl/space/components/gravity/GravPullComponent.java
@@ -1,7 +1,10 @@
 package atl.space.components.gravity;
 
 public abstract class GravPullComponent implements GravPullable{
-  public double baseGravMass;
+  //public double baseGravMass;
+  public abstract double getBaseMass(); //Decided that mass be separate from gravity components so mass can be used
+                                        //by stuff like acceleration calculators
+  
   public GravPullComponent(){
     this(1);
   }
