d52a68a3fe8e1d40447fe5b4ed77e09ff07e73f3
addtheletters <bzhang.2003@gmail.com>
2013-05-21 16:32:19 -0300

    Update GravPullComponent.java

--- src/atl/space/components/gravity/GravPullComponent.java
+++ src/atl/space/components/gravity/GravPullComponent.java
@@ -2,18 +2,15 @@ package atl.space.components.gravity;
 
 public abstract class GravPullComponent implements GravPullable{
   //public double baseGravMass;
-  public abstract double getBaseMass(); //Decided that mass be separate from gravity components so mass can be used
+  public abstract double getBaseGravMass(); //Decided that mass be separate from gravity components so mass can be used
                                         //by stuff like acceleration calculators
   
   public GravPullComponent(){
-    this(1);
-  }
-  public GravPullComponent(double mass){
     super("gravpull");
-    this.baseGravMass = mass;
   }
+  
   public double getGravMass(){
-    return baseGravMass * getMassModifier();
+    return getBaseGravMass() * getMassModifier();
   }
   private abstract double getMassModifier();
   
