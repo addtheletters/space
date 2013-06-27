78daf8212437ea8ac0d1001c4b56374b43f794d8
lexical_enumeration <bzhang.2003@gmail.com>
2013-06-26 22:30:26 -0400

    radiation: patching up emissions?

--- src/atl/space/components/radiation/Emission.java
+++ src/atl/space/components/radiation/Emission.java
@@ -4,18 +4,12 @@ public class Emission {
 	private int wavelength;
 	//multiple wavelengths? huh... how 2 do
 	
-	private EmissionPattern pattern;
 	public int getWavelength() {
 		return wavelength;
 	}
 	public void setWavelength(int wavelength) {
 		this.wavelength = wavelength;
 	}
-	public EmissionPattern getPattern() {
-		return pattern;
-	}
-	public void setPattern(EmissionPattern pattern) {
-		this.pattern = pattern;
-	}
+	
 	
 }
--- src/atl/space/components/radiation/EmissionPattern.java
+++
@@ -1,5 +0,0 @@
-package atl.space.components.radiation;
-
-public class EmissionPattern {
-	//Noise, a transmission, scanning pattern, pulsing, etc?
-}
